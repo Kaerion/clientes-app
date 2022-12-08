0f8e722c1b8cb32d8c6d060f0262281ec446d4e6
Kaerion <jquero89@gmail.com>
2022-12-08 11:59:04 +0100

    Delete

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
 
@@ -19,4 +20,30 @@ export class ClientesComponent implements OnInit {
     );
   }
 
+  delete(cliente : Cliente): void {
+    Swal.fire({
+      title: 'Esta seguro?',
+      text: "Este cambio no puede revertirse!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Eliminar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.value) {
+        this.clienteService.delete(cliente.id).subscribe(
+        response => {
+          this.clientes = this.clientes?.filter(cli => cli !== cliente);
+          Swal.fire(
+            'Eliminado!',
+            `El cliente ${cliente.nombre} ha sido eliminado.`,
+            'success'
+          );
+        });
+      }
+    });
+
+    
+  }
 }
